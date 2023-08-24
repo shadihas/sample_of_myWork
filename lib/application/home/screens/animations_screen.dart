3931e4a50dfddca186a7055940d6a02776ec20ce
Shadi Hasan <shadihas2@gmail.com>
2023-08-24 23:04:53 +0300

    modified the animations screen design

--- lib/application/home/screens/animations_screen.dart
+++ lib/application/home/screens/animations_screen.dart
@@ -15,84 +15,90 @@ class AnimationsScreen extends StatelessWidget {
     return Scaffold(
       appBar: customAppBar(title: "Animations"),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Padding(
-              padding: EdgeInsets.only(top: 40.h),
-              child: BlocBuilder<TextHomeBloc, TextHomeState>(
-                builder: (context, state) {
-                  return Text(
-                    state.text.isEmpty ? "Your name" : state.text,
-                    style:
-                        AppFontStyle.appTextStyle(color: AppColors.blackColor),
-                  );
-                },
-              ),
-            ),
-            BlocBuilder<AnimationsBloc, AnimationsState>(
-              builder: (context, state) {
-                return AnimatedContainer(
-                  duration: Duration(milliseconds: 500),
-                  width: containerSize,
-                  height: containerSize,
-                  decoration: BoxDecoration(
-                    color: state.containerColor,
-                    borderRadius: state.containerBorderRadius,
+        child: SingleChildScrollView(
+          child: SizedBox(
+            height: Dimensions.screenHeight - 80,
+            child: Column(
+               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 40.h),
+                  child: BlocBuilder<TextHomeBloc, TextHomeState>(
+                    builder: (context, state) {
+                      return Text(
+                        state.text.isEmpty ? "Your name" : state.text,
+                        style:
+                            AppFontStyle.appTextStyle(color: AppColors.blackColor),
+                      );
+                    },
                   ),
-                );
-              },
-            ),
-            SizedBox(height: 20.h),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 40.h, horizontal: 30.w),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  GestureDetector(
-                    onTap: () => animationsBloc.add(AnimationsChangedEvent(
-                        containerBorderRadius: BorderRadius.circular(0),
-                        containerColor: AppColors.darkBlueColor)),
-                    child: Container(
-                      width: 70.w,
-                      height: 70.w,
+                ),
+           
+                BlocBuilder<AnimationsBloc, AnimationsState>(
+                  builder: (context, state) {
+                    return AnimatedContainer(
+                      duration: Duration(milliseconds: 500),
+                      width: containerSize,
+                      height: containerSize,
                       decoration: BoxDecoration(
-                        color: AppColors.darkBlueColor,
-                        borderRadius: BorderRadius.circular(0),
+                        color: state.containerColor,
+                        borderRadius: state.containerBorderRadius,
                       ),
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: () => animationsBloc.add(AnimationsChangedEvent(
-                        containerBorderRadius: BorderRadius.circular(40),
-                        containerColor: AppColors.lightBlueColor)),
-                    child: Container(
-                      width: 70.w,
-                      height: 70.w,
-                      decoration: BoxDecoration(
-                        color: AppColors.lightBlueColor,
-                        borderRadius: BorderRadius.circular(15),
+                    );
+                  },
+                ),
+                SizedBox(height: 20.h,),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 40.h, horizontal: 30.w),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      GestureDetector(
+                        onTap: () => animationsBloc.add(AnimationsChangedEvent(
+                            containerBorderRadius: BorderRadius.circular(0),
+                            containerColor: AppColors.darkBlueColor)),
+                        child: Container(
+                          width: 70.w,
+                          height: 70.w,
+                          decoration: BoxDecoration(
+                            color: AppColors.darkBlueColor,
+                            borderRadius: BorderRadius.circular(0),
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: () => animationsBloc.add(AnimationsChangedEvent(
-                        containerBorderRadius:
-                            BorderRadius.circular(containerSize / 2),
-                        containerColor: AppColors.lightRedColor)),
-                    child: Container(
-                      width: 70.w,
-                      height: 70.w,
-                      decoration: BoxDecoration(
-                        color: AppColors.lightRedColor,
-                        borderRadius: BorderRadius.circular(35.w),
+                      GestureDetector(
+                        onTap: () => animationsBloc.add(AnimationsChangedEvent(
+                            containerBorderRadius: BorderRadius.circular(40),
+                            containerColor: AppColors.lightBlueColor)),
+                        child: Container(
+                          width: 70.w,
+                          height: 70.w,
+                          decoration: BoxDecoration(
+                            color: AppColors.lightBlueColor,
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                        ),
                       ),
-                    ),
+                      GestureDetector(
+                        onTap: () => animationsBloc.add(AnimationsChangedEvent(
+                            containerBorderRadius:
+                                BorderRadius.circular(containerSize / 2),
+                            containerColor: AppColors.lightRedColor)),
+                        child: Container(
+                          width: 70.w,
+                          height: 70.w,
+                          decoration: BoxDecoration(
+                            color: AppColors.lightRedColor,
+                            borderRadius: BorderRadius.circular(35.w),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
--- lib/application/home/screens/home_screen.dart
+++ lib/application/home/screens/home_screen.dart
@@ -15,75 +15,79 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     TextEditingController homeTextEditingController = TextEditingController();
-    return Scaffold(
-      appBar: customAppBar(title: "Home"),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              // 80 is the appBar height
-              height: (Dimensions.screenHeight / 2) - 80,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(
-                        top: 50.h, bottom: 50.h, left: 20.w, right: 20.w),
-                    child: CustomTextField(
-                      onChanged: (text) {
-                        BlocProvider.of<TextHomeBloc>(context)
-                            .add(TextHomeTypedEvent(text: text));
-                      },
-                      homeTextEditingController: homeTextEditingController,
-                      hintText: "Enter your name",
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        appBar: customAppBar(title: "Home"),
+        body: SingleChildScrollView(
+          child: SizedBox(
+            // 80 is the app bar height
+            height: Dimensions.screenHeight-80,
+            child: Column(
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(
+                          top: 50.h, bottom: 50.h, left: 20.w, right: 20.w),
+                      child: CustomTextField(
+                        onChanged: (text) {
+                          BlocProvider.of<TextHomeBloc>(context)
+                              .add(TextHomeTypedEvent(text: text));
+                        },
+                        homeTextEditingController: homeTextEditingController,
+                        hintText: "Enter your name",
+                      ),
                     ),
+                    BlocBuilder<TextHomeBloc, TextHomeState>(
+                      builder: (context, state) {
+                        return Text(
+                         state.text.isEmpty ? "Your name" : state.text,
+                          style: AppFontStyle.appTextStyle(
+                              color: AppColors.blackColor),
+                        );
+                      },
+                    ),
+                    SizedBox(height: 20.h),
+                  ],
+                ),
+               
+                Flexible(
+                  fit: FlexFit.loose,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                       ClearTextButton(
+                        onTap: () {
+                           homeTextEditingController.clear();
+                          BlocProvider.of<TextHomeBloc>(context).add(TextHomeClearedEvent());
+                        },
+                      ),
+                      SizedBox(height: 20.h),
+                      CustomHomeButton(
+                        onTap: () {
+                          Navigator.of(context)
+                              .pushNamed(AppRouter.animationsScreen);
+                        },
+                        buttonColor: AppColors.darkBlueColor,
+                        text: "Go to page 1",
+                      ),
+                      SizedBox(height: 20.h),
+                      CustomHomeButton(
+                        onTap: () {
+                          Navigator.of(context).pushNamed(AppRouter.pokemonsScreen);
+                        },
+                        buttonColor: AppColors.lightBlueColor,
+                        text: "Go to page 2",
+                      ),
+                      SizedBox(height: 20.h),
+                    ],
                   ),
-                  BlocBuilder<TextHomeBloc, TextHomeState>(
-                    builder: (context, state) {
-                      return Text(
-                       state.text.isEmpty ? "Your name" : state.text,
-                        style: AppFontStyle.appTextStyle(
-                            color: AppColors.blackColor),
-                      );
-                    },
-                  ),
-                  SizedBox(height: 20.h),
-                ],
-              ),
+                ),
+              ],
             ),
-            SizedBox(
-              height: (Dimensions.screenHeight / 2),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                   ClearTextButton(
-                    onTap: () {
-                       homeTextEditingController.clear();
-                      BlocProvider.of<TextHomeBloc>(context).add(TextHomeClearedEvent());
-                    },
-                  ),
-                  SizedBox(height: 20.h),
-                  CustomHomeButton(
-                    onTap: () {
-                      Navigator.of(context)
-                          .pushNamed(AppRouter.animationsScreen);
-                    },
-                    buttonColor: AppColors.darkBlueColor,
-                    text: "Go to page 1",
-                  ),
-                  SizedBox(height: 20.h),
-                  CustomHomeButton(
-                    onTap: () {
-                      Navigator.of(context).pushNamed(AppRouter.pokemonsScreen);
-                    },
-                    buttonColor: AppColors.lightBlueColor,
-                    text: "Go to page 2",
-                  ),
-                  SizedBox(height: 20.h),
-                ],
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
